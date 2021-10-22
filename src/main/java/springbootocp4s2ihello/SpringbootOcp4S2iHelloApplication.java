1f998edba0df7053df89b2ba3a4919e56eebe47b
Maqsood Alam <malam84@gmail.com>
2021-10-22 16:12:34 +0800

    Springboot Hello world deployment on ocp4 using s2i template

---
+++ src/main/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplication.java
@@ -0,0 +1,23 @@
+package springbootocp4s2ihello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootOcp4S2iHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOcp4S2iHelloApplication.class, args);
+	}
+	
+	
+	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	public String getHello() {
+		return "Hello";
+	}
+
+}
---
+++ src/test/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootocp4s2ihello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOcp4S2iHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
