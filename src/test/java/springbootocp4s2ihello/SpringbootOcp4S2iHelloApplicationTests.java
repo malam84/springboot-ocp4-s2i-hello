86faf92311878b205ab39c6b046a15ac996b4b03
Maqsood Alam <malam84@gmail.com>
2021-11-13 15:02:56 +0800

    Update SpringbootOcp4S2iHelloApplicationTests.java

--- src/test/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplicationTests.java
+++ src/test/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootOcp4S2iHelloApplicationTests {
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+ 	@Test
+ 	void contextLoads() {
+ 	}
 
 }
