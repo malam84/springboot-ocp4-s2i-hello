982df205dabdb878af46958dd0a36339465fd749
Maqsood Alam <malam84@gmail.com>
2023-06-21 14:19:58 +0800

    Update SpringbootOcp4S2iHelloApplication.java

--- src/main/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplication.java
+++ src/main/java/springbootocp4s2ihello/SpringbootOcp4S2iHelloApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @SpringBootApplication
